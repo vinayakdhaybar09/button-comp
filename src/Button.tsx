8f0995f53adb9a281cdf370d032ef8bed86e722d
unknown <vinayakdhaybar09@gmail.com>
2024-04-01 17:13:37 +0530

    first commit

---
+++ src/Button.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { ReactNode } from "react";
+
+type PropsType = {
+  children: ReactNode;
+  onClick?: () => void;
+};
+
+// this is a comman component
+
+const Button = ({ children, onClick }: PropsType) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        padding: "10px 20px",
+        fontSize: "1.2em",
+        borderRadius: "5px",
+        cursor: "pointer",
+        backgroundColor: "blue",
+        color: "white",
+        border: "none",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export { Button };
